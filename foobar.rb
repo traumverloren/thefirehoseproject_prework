e6fa00f642f5af4ca738594dd0c313e83dcfb15f
Stephanie Nemeth <traumverloren@gmail.com>
2015-01-26 13:16:41 +0100

    foobar challenge

---
+++ foobar.rb
@@ -0,0 +1,27 @@
+# Foobar challenge problem:
+# foo if number divisible by 3
+# bar if number divisible by 5
+# foobar if divisible by 3 & 5!
+# count up to user specified number!
+
+puts "Enter a number to play the foobar game!"
+number = gets.chomp.to_i
+if number <= 0
+  puts "Sorry, please enter a number greater than 0."
+  number = gets.chomp.to_i
+end
+puts "Here's your results:"
+count = 1
+while count <= number
+  if count % 3 == 0 && count % 5 == 0
+    puts "foobar"
+  elsif count % 3 == 0
+    puts "foo"
+  elsif count % 5 == 0
+    puts "bar"
+  else
+    puts "#{count}"
+  end
+
+  count += 1
+end
